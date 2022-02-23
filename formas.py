416b25305590906c3cf81d6d71afe68e84713a53
raulsg <raulsorgo@gmail.com>
2022-02-23 10:46:42 +0100

    primera version

--- formas.py
+++ formas.py
@@ -1,5 +1,8 @@
 from math import pi
 
+class RadioError(Exception):
+    pass
+
 
 class Forma():
     
@@ -12,12 +15,34 @@ class Forma():
     def __str__(self) -> str:
         return type(self).__name__
 
+
 class Circulo(Forma):
     def __init__(self,radio) -> None:
-        self.__radio = radio
+        if type(radio) in (int, float) and radio >=0: 
+            self.__radio = radio
+        else:
+            raise Exception('El radio debe der un número positivo')
 
     def area(self):
         return pi * self.__radio * self.__radio
 
     def perimetro(self):
         return 2 * pi * self.__radio
+
+
+class Rectangulo(Forma):
+    def __init__(self, base, altura) -> None:
+        self.__base = base 
+        self.__altura = altura
+
+    def area(self):
+        return self.__base * self.__altura
+
+    def perimetro(self):
+        return 2 * (self.__base + self.__altura)
+
+class Cuadrado(Rectangulo):
+    def __init__(self, lado) -> None:
+        super().__init__(lado, lado )
+
+    
--- test_formas.py
+++ test_formas.py
@@ -1,5 +1,6 @@
+from math import pi
 import unittest
-from formas import Circulo, Forma
+from formas import Circulo, Cuadrado, Forma, RadioError
 
 class TestFormas(unittest.TestCase):
     def test_clase_forma_existe(self):
@@ -12,5 +13,26 @@ class TestFormas(unittest.TestCase):
 
 class TestCirculo(unittest.TestCase):
     def test_clase_circulo_existe(self):
-        c = Circulo()
+        c = Circulo(1)
         self.assertIsNotNone(c)
+
+    def test_circulo_radio1_perimetro_2pi(self):
+        c = Circulo(1)
+        peri = c.perimetro()
+        self.assertEqual(peri,2*pi)
+
+    def test_radio_negativo_da_error(self):
+        with self.assertRaises(Exception):
+            c = Circulo(-1)
+
+class TestCudrado(unittest.TestCase):
+
+    def test_lado_1_perimetro_4(self):
+        c = Cuadrado(1)
+        peri = c.perimetro()
+        self.assertEqual(peri,4)
+
+    def test_area_lado_1_1(self):
+        c = Cuadrado(1)
+        a = c.area()
+        self.assertEqual(a,1)
